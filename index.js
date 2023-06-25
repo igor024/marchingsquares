458f91f2ecc1ab6315242f3bdffcb724b0ed040c
alexfinger21 <cdx9933@gmail.com>
2023-06-25 17:27:30 -0400

    nice

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const pointDist = 10
-
-const canvas = document.getElementById("canvas")
-
 function main() {
+    const pointDist = 10
+    const canvas = document.getElementById("canvas").getContext("2d")
+
     //generate seed
 
     //draw
+    const pointsX = Math.floor(canvas.width/pointDist) + 1
+    const pointsY = Math.floor(canvas.height/pointDist) + 1
+
+    canvas
 
 }
 
